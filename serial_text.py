843f7c704bb6faaf76d391a21177ae3f893ac6ca
Michael Malinowski <mike.m7@hotmail.com>
2019-02-08 05:10:57 -0500

    Update serial_text.py

--- serial_text.py
+++ serial_text.py
@@ -1,20 +1,25 @@
 import serial
+
 ser = serial.Serial()
 ser.baudrate = 9600
+#Make sure to change ser.port to your correct port.
 ser.port = "COM6"
 ser.open()
 ser.is_open
 data = []
+stopper = "E"
+#The stopper variable ends the loop and saves everythin before that point to the text file
 while 1:
     line = ser.readline()
     new_line = line.decode("utf-8")
     print(new_line)
-    if "E" in new_line:
+    if  in new_line:
         break;
     data.append(new_line)
 ser.close()
 
-x = input("Name of file: ")
+#No need to include the extension
+x = input("Name of file: " + ".txt")
 f = open(x, "w")
 for p in data:
     f.write(p)
